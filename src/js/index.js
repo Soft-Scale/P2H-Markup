d12a340d0aebf16983c9097f2b136780bb563258
Stanislav Orekhovskiy <stanislav.orekhovskiy@gmail.com>
2021-01-28 13:49:29 +0200

    Add markup mobile version

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,69 @@
 // new Splide( '.splide' ).mount();
-new Splide( '#splide', {
-    type   : 'loop',
+new Splide('#splide', {
+    type: 'loop',
     perPage: 3,
     perMove: 1,
-    width: '980px',
+    gap: '50px',
+    width: '920px',
     pagination: false,
-	focus  : 'center',
-} ).mount();
+    focus: 'center',
+    breakpoints: {
+		1180: {
+            width: '860px',
+            gap: '20px',
+            perPage: 3,
+            focus: 1,
+        },
+        1024: {
+            width: '540px',
+            gap: '20px',
+            perPage: 2,
+            focus: 1,
+        },
+        680: {
+            width: '270px',
+            // gap: '30px',
+            perPage: 1,
+            focus: 1,
+        },
+        480: {
+            width: '230px',
+            gap: '0',
+            perPage: 1,
+            focus: 1,
+		},
+	}
+}).mount();
 
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 
 for (let smoothLink of smoothLinks) {
-    console.log(smoothLinks)
     smoothLink.addEventListener('click', function (e) {
         e.preventDefault();
         const id = smoothLink.getAttribute('href');
-        console.log(id)
 
-        
+
         document.querySelector(id).scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         });
     });
 };
+
+(function() {
+
+    var hamburger = {
+      navToggle: document.querySelector('.nav-toggle'),
+      nav: document.querySelector('nav'),
+  
+      doToggle: function(e) {
+        e.preventDefault();
+        this.navToggle.classList.toggle('expanded');
+        this.nav.classList.toggle('expanded');
+      }
+    };
+  
+    hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+    hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+  
+  }());
